6ae81f11d39e03df9701ed255c2d3299b223f588
Kegrathil <willkegrathil@gmail.com>
2023-10-27 20:16:08 +0200

    5 visualizations

--- sakila_visualizations.py
+++ sakila_visualizations.py
@@ -6,57 +6,161 @@ pss = ""
 
 # Replace 'username' and 'password' with your MySQL username and password
 engine = create_engine(f'mysql://root:{pss}@localhost/sakila')
-query = """
-SELECT c.name AS category, COUNT(r.rental_id) AS rental_count
-FROM category c
-JOIN film_category fc ON c.category_id = fc.category_id
-JOIN film f ON fc.film_id = f.film_id
-JOIN inventory i ON f.film_id = i.film_id
-JOIN rental r ON i.inventory_id = r.inventory_id
-GROUP BY category;
-"""
 
-# Execute the SQL query and load the results into a pandas DataFrame
-rental_data = pd.read_sql(query, engine)
-
-
-# Set the figure size
-plt.figure(figsize=(10, 6))
-
-# Create a bar chart
-plt.bar(rental_data['category'], rental_data['rental_count'])
-
-# Customize the chart
-plt.title('Number of Rentals by Category')
-plt.xlabel('Category')
-plt.ylabel('Rental Count')
-plt.xticks(rotation=45)  # Rotate category labels for readability
-
-# Display the chart
-plt.tight_layout()
-plt.show()
-
-query = """
-SELECT DATE(rental_date) AS rental_day, COUNT(rental_id) AS rental_count
-FROM rental
-GROUP BY rental_day;
-"""
-
-# Execute the SQL query and load the results into a pandas DataFrame
-rental_data = pd.read_sql(query, engine)
-
-# Set the figure size
 plt.figure(figsize=(12, 6))
 
-# Create a time-series line chart
-plt.plot(rental_data['rental_day'], rental_data['rental_count'], marker='o', linestyle='-')
 
-# Customize the chart
-plt.title('Rental Count Over Time')
-plt.xlabel('Rental Day')
-plt.ylabel('Rental Count')
-plt.xticks(rotation=45)  # Rotate x-axis labels for readability
+###############################################################################################
+plt.title('Top Film Revenues')
 
-# Display the chart
-plt.tight_layout()
+query = """
+SELECT 
+    f.film_id,
+    f.title AS film_title,
+    SUM(p.amount) AS total_revenue
+FROM
+    film AS f
+        JOIN
+    inventory AS i ON f.film_id = i.film_id
+        JOIN
+    rental AS r ON i.inventory_id = r.inventory_id
+        JOIN
+    payment AS p ON r.rental_id = p.rental_id
+GROUP BY f.film_id , film_title
+ORDER BY total_revenue DESC
+LIMIT 5;
+"""
+
+rental_data = pd.read_sql(query, engine)
+plt.bar(rental_data['film_title'], rental_data['total_revenue'])
+
+plt.show()
+
+
+###############################################################################################
+plt.title('Top Actors')
+
+query = """
+SELECT 
+    concat(a.first_name, " ", a.last_name) as actor_name, COUNT(fa.film_id) as films
+FROM
+    actor AS a
+        JOIN
+    film_actor AS fa ON a.actor_id = fa.actor_id
+GROUP BY a.first_name , a.last_name
+HAVING films > 15
+ORDER BY films DESC;
+"""
+
+rental_data = pd.read_sql(query, engine)
+plt.bar(rental_data['actor_name'], rental_data['films'])
+
+plt.show()
+
+
+###############################################################################################
+plt.title('CustomerPayments')
+
+query = """
+SELECT 
+    c.customer_id,
+    CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
+    SUM(p.amount) AS total_payments
+FROM
+    customer AS c
+        JOIN
+    payment AS p ON c.customer_id = p.customer_id
+GROUP BY c.customer_id , customer_name
+ORDER BY total_payments DESC;
+"""
+
+rental_data = pd.read_sql(query, engine)
+plt.bar(rental_data['customer_id'], rental_data['total_payments'])
+
+plt.show()
+
+
+###############################################################################################
+plt.title('Top Film Revenues')
+
+query = """
+SELECT
+    category_name,
+    film_title,
+    rental_count
+FROM (
+    SELECT
+        c.name AS category_name,
+        f.title AS film_title,
+        COUNT(*) AS rental_count,
+        ROW_NUMBER() OVER (PARTITION BY c.name ORDER BY COUNT(*) DESC) AS p
+    FROM
+        category AS c
+    JOIN
+        film_category AS fc
+        ON c.category_id = fc.category_id
+    JOIN
+        film AS f
+        ON fc.film_id = f.film_id
+    JOIN
+        inventory AS i
+        ON f.film_id = i.film_id
+    JOIN
+        rental AS r
+        ON i.inventory_id = r.inventory_id
+    GROUP BY
+        c.name, f.title
+) AS RankedFilms
+WHERE
+    p <= 3
+ORDER BY
+    category_name, p;
+"""
+
+rental_data = pd.read_sql(query, engine)
+plt.bar(rental_data['film_title'], rental_data['rental_count'])
+
+plt.show()
+
+
+###############################################################################################
+plt.title('Categories over average length')
+
+query = """
+WITH CategoryAvgLength AS (
+    SELECT
+        c.name AS category_name,
+        AVG(f.length) AS avg_length
+    FROM
+        film AS f
+    JOIN
+        film_category AS fc
+        ON f.film_id = fc.film_id
+    JOIN
+        category AS c
+        ON fc.category_id = c.category_id
+    GROUP BY
+        c.name
+),
+OverallAvgLength AS (
+    SELECT
+        AVG(length) AS overall_avg_length
+    FROM
+        film
+)
+
+SELECT
+    cat.category_name,
+    cat.avg_length
+FROM
+    CategoryAvgLength AS cat
+JOIN
+    OverallAvgLength AS overall
+WHERE
+    cat.avg_length > overall.overall_avg_length;
+"""
+
+rental_data = pd.read_sql(query, engine)
+plt.bar(rental_data['category_name'], rental_data['avg_length'])
+
 plt.show()
